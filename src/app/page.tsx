fc078974d8397bcba596d3b6f050776279de1c91
Adam Stępień <vesemir2k@gmail.com>
2024-10-22 22:46:00 +0200

    ✨ (page.tsx): Add new sections with specific styles to improve the structure and layout of the Home page 🔧 (home.module.scss): Add new styles for the newly created sections on the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,71 @@
 import Card from "./components/Card"
 import ImageCard from "./components/ImageCard"
+import styles from "@/app/styles/components/home/home.module.scss"
 
 export default function Home() {
   return (
     <main>
-      <Card
-        srcImage="/assets/home/mobile/create-and-share.jpg"
-        title="Create and share your photo stories"
-        description="Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
-        textButton="GET AN INVITE"
-        hrefButton="/"
-        theme="dark"
-        accent={true}
-      />
-      <Card
-        srcImage="/assets/home/mobile/beautiful-stories.jpg"
-        title="Beautiful stories ever time"
-        description="We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone."
-        textButton="View the stories"
-        hrefButton="/"
-        theme="light"
-      />
-      <Card
-        srcImage="/assets/home/mobile/designed-for-everyone.jpg"
-        title="Designed for everyone"
-        description="Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it. "
-        textButton="View the stories"
-        hrefButton="/"
-        theme="light"
-      />
+      <section className={styles["section-hero"]}>
+        <Card
+          srcImage="/assets/home/mobile/create-and-share.jpg"
+          title="Create and share your photo stories"
+          description="Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
+          textButton="GET AN INVITE"
+          hrefButton="/"
+          theme="dark"
+          accent={true}
+        />
+      </section>
 
-      <ImageCard
-        srcImage="/assets/stories/mobile/mountains.jpg"
-        title="The Mountains"
-        author="John Appleseed"
-        textButton="Read Store"
-        hrefButton="/"
-      />
-      <ImageCard
-        srcImage="/assets/stories/mobile/cityscapes.jpg"
-        title="Sunset Cityscapes"
-        author="Benjamin Cruz"
-        textButton="Read Store"
-        hrefButton="/"
-      />
-      <ImageCard
-        srcImage="/assets/stories/mobile/18-days-voyage.jpg"
-        title="18 Days Voyage"
-        author="Alexei Borodin"
-        textButton="Read Store"
-        hrefButton="/"
-      />
-      <ImageCard
-        srcImage="/assets/stories/mobile/architecturals.jpg"
-        title="Architecturals"
-        author="Samantha Brooke"
-        textButton="Read Store"
-        hrefButton="/"
-      />
+      <section className={styles["section-view-stories"]}>
+        <Card
+          srcImage="/assets/home/mobile/beautiful-stories.jpg"
+          title="Beautiful stories ever time"
+          description="We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone."
+          textButton="View the stories"
+          hrefButton="/"
+          theme="light"
+        />
+        <Card
+          srcImage="/assets/home/mobile/designed-for-everyone.jpg"
+          title="Designed for everyone"
+          description="Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it. "
+          textButton="View the stories"
+          hrefButton="/"
+          theme="light"
+        />
+      </section>
+
+      <section className={styles["section-stories"]}>
+        <ImageCard
+          srcImage="/assets/stories/mobile/mountains.jpg"
+          title="The Mountains"
+          author="John Appleseed"
+          textButton="Read Story"
+          hrefButton="/"
+        />
+        <ImageCard
+          srcImage="/assets/stories/mobile/cityscapes.jpg"
+          title="Sunset Cityscapes"
+          author="Benjamin Cruz"
+          textButton="Read Story"
+          hrefButton="/"
+        />
+        <ImageCard
+          srcImage="/assets/stories/mobile/18-days-voyage.jpg"
+          title="18 Days Voyage"
+          author="Alexei Borodin"
+          textButton="Read Story"
+          hrefButton="/"
+        />
+        <ImageCard
+          srcImage="/assets/stories/mobile/architecturals.jpg"
+          title="Architecturals"
+          author="Samantha Brooke"
+          textButton="Read Story"
+          hrefButton="/"
+        />
+      </section>
     </main>
   )
 }
