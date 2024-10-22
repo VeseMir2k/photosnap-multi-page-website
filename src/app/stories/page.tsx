76a83845383882f56e0999f5ff128a2c6f84ac1c
Adam Stępień <vesemir2k@gmail.com>
2024-10-22 03:06:49 +0200

    ✨ (page.tsx): Add a list of stories with details like date, title, author, and image to enhance the Stories component functionality.

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,3 +1,109 @@
+import ImageCard from "../components/ImageCard"
+
+const storiesData = [
+  {
+    date: "April 16th 2020",
+    title: "The Mountains",
+    author: "John Appleseed",
+    image: "/assets/stories/mobile/mountains.jpg",
+  },
+  {
+    date: "April 14th 2020",
+    title: "Sunset Cityscapes",
+    author: "Benjamin Cruz",
+    image: "/assets/stories/mobile/cityscapes.jpg",
+  },
+  {
+    date: "April 11th 2020",
+    title: "18 Days Voyage",
+    author: "Alexei Borodin",
+    image: "/assets/stories/mobile/18-days-voyage.jpg",
+  },
+  {
+    date: "April 9th 2020",
+    title: "Architecturals",
+    author: "Samantha Brooke",
+    image: "/assets/stories/mobile/architecturals.jpg",
+  },
+  {
+    date: "April 7th 2020",
+    title: "World Tour 2019",
+    author: "Timothy Wagner",
+    image: "/assets/stories/mobile/world-tour.jpg",
+  },
+  {
+    date: "April 3rd 2020",
+    title: "Unforeseen Corners",
+    author: "William Malcolm",
+    image: "/assets/stories/mobile/unforeseen-corners.jpg",
+  },
+  {
+    date: "March 29th 2020",
+    title: "King on Africa: Part II",
+    author: "Tim Hillenburg",
+    image: "/assets/stories/mobile/king-of-africa.jpg",
+  },
+  {
+    date: "March 21st 2020",
+    title: "The Trip to Nowhere",
+    author: "Felicia Rourke",
+    image: "/assets/stories/mobile/trip-to-nowhere.jpg",
+  },
+  {
+    date: "March 19th 2020",
+    title: "Rage of The Sea",
+    author: "Mohammed Abdul",
+    image: "/assets/stories/mobile/rage-of-the-sea.jpg",
+  },
+  {
+    date: "March 16th 2020",
+    title: "Running Free",
+    author: "Michelle",
+    image: "/assets/stories/mobile/running-free.jpg",
+  },
+  {
+    date: "March 11th 2020",
+    title: "Behind the Waves",
+    author: "Lamarr Wilson",
+    image: "/assets/stories/mobile/behind-the-waves.jpg",
+  },
+  {
+    date: "March 9th 2020",
+    title: "Calm Waters",
+    author: "Samantha Brooke",
+    image: "/assets/stories/mobile/calm-waters.jpg",
+  },
+  {
+    date: "March 5th 2020",
+    title: "The Milky Way",
+    author: "Benjamin Cruz",
+    image: "/assets/stories/mobile/milky-way.jpg",
+  },
+  {
+    date: "March 1st 2020",
+    title: "Somwarpet’s Beauty",
+    author: "Michelle",
+    image: "/assets/stories/mobile/somwarpet.jpg",
+  },
+  {
+    date: "February 25th 2020",
+    title: "Land of Dreams",
+    author: "William Malcolm",
+    image: "/assets/stories/mobile/land-of-dreams.jpg",
+  },
+]
+
 export default function Stories() {
-  return <h2>Stories</h2>
+  const stories = storiesData.map((item) => (
+    <ImageCard
+      key={item.title}
+      srcImage={item.image}
+      date={item.date}
+      title={item.title}
+      author={item.author}
+      textButton="Read story"
+      hrefButton="/"
+    />
+  ))
+  return <section>{stories}</section>
 }
