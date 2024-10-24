c2a103109cc2ca3e0fbee19aa8155e3aa4820fdd
Adam Stępień <vesemir2k@gmail.com>
2024-10-24 13:37:50 +0200

    ✨ (Overlay.tsx): add functionality to close the menu when clicking on the overlay to improve user experience

--- src/app/components/Overlay.tsx
+++ src/app/components/Overlay.tsx
@@ -3,7 +3,12 @@ import { useStore } from "../stores/store"
 import styles from "@/app/styles/components/overlay.module.scss"
 
 export default function Overlay() {
-  const { isMenuOpen } = useStore()
+  const { isMenuOpen, closeMenu } = useStore()
 
-  return isMenuOpen ? <div className={styles["overlay"]}></div> : null
+  return isMenuOpen ? (
+    <div
+      onClick={closeMenu}
+      className={styles["overlay"]}
+    ></div>
+  ) : null
 }
